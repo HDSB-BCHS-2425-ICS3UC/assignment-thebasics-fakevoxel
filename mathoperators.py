958d42d356fb274559cb0169c900e3222ebe3b1c
Maximilian McDiarmid <maximilianmcdiarmid@icloud.com>
2025-03-03 13:51:00 -0500

    added more functions for cone and cylinder, plus respective print statements

--- mathoperators.py
+++ mathoperators.py
@@ -4,19 +4,31 @@ import math
 #--------------------------------------
 
 # formula for area of a cube is width^3
-def findAreaOfCube(width):
+def findVolumeOfCube(width):
     return width * width * width
 
-#formula for area of a sphere is (4/3)(pi)(radius^3)
-def findAreaOfSphere(radius):
+# formula for area of a sphere is (4/3)(pi)(radius^3)
+def findVolumeOfSphere(radius):
     return (4 / 3) * radius * radius * radius * math.pi
+
+# formula for area of a cone is (1/3)(pi)(radius^2)(height)
+def findVolumeOfCone(radius, height):
+    return (1/3) * math.pi * radius * radius * height
+
+# formula for area of a cylinder is (pi)(height)(radius^2)
+def findVolumeOfCylinder(radius, height):
+    return math.pi * radius * radius * height
 
 # defining arbitrary parameters for the shapes we want to calculate
 # these will be set via user input soon
 radius = 1
 width = 5
+height = 2
 
-print("The area of a cube with side length " + str(width) + " is " + str(findAreaOfCube(5)) + ".")
+print("The area of a cube with side length " + str(width) + " is " + str(findVolumeOfCube(width)) + ".")
 
+print("The area of a sphere with radius " + str(radius) + " is " + str(findVolumeOfSphere(radius)) + ".")
 
-print("The area of a sphere with radius " + str(radius) + " is " + str(findAreaOfSphere(5)) + ".")
+print("The area of a cone with radius " + str(radius) + " and height " + str(height) + " is " + str(findVolumeOfCone(radius, height)) + ".")
+
+print("The area of a cylinder with radius " + str(radius) + " and height " + str(height) + " is " + str(findVolumeOfCylinder(radius, height)) + ".")
